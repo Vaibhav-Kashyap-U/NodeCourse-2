dec88aa7bb6290544053a2d183ddbdc563d5a0f5
Vaibhav Kashyap U <mail.kashyap13@gmail.com>
2019-01-04 15:47:44 +0530

    Completed all Routes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://vaibhav:vaibhav13@ds149344.mlab.com:49344/workdb' || 'mongodb: //localhost:27017/TodoApp', {
-    useNewUrlParser: true
-});
+mongoose.connect('mongodb://vaibhav:vaibhav13@ds149344.mlab.com:49344/workdb' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
