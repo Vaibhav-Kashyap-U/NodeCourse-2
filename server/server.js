df97ba59277044e070db266d2881de14365417e9
Vaibhav Kashyap U <mail.kashyap13@gmail.com>
2019-01-04 12:36:01 +0530

    Testing done

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const bodyParser = require ('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -9,18 +10,46 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
-    var todo = new Todo({
-        text : req.body.text
-    });
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    todo.save().then((doc)=>{
-        res.status(200).send(`The following Todo ${JSON.stringify(doc,undefined,2)} is saved`);
-    },(error)=>{
-        res.status(400).send(`Bad request - ${error}`)
-    });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-app.listen(3000,()=>{
-    console.log('Server listening on Port 3000');
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+module.exports = {app};
