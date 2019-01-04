f1aff647b8be5e6532b7331ff98d56afca498949
Vaibhav Kashyap U <mail.kashyap13@gmail.com>
2019-01-04 16:22:00 +0530

    final commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://vaibhav:vaibhav13@ds149344.mlab.com:49344/workdb' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+});
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
+var env = process.env.NODE_ENV || 'development'
+console.log('env *****' ,env);
+
+if(env === 'development'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else if(env === 'production'){
+    process.env.MONGODB_URI = 'mongodb://vaibhav:vaibhav13@ds149344.mlab.com:49344/workdb';
+} else console.log('Environment variable undefined');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,8 +21,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
